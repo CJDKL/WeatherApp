06522bbeff6fcb4186ca405127ee529865a74739
CJDKL <junchang1124@gmail.com>
2020-03-03 23:09:52 -0800

    Set up PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'WeatherApp',
+    script: './server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-34-217-101-114.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/WeatherApp.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:CJDKL/WeatherApp.git',
+      path: '/home/ubuntu/WeatherApp',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
